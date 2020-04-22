b028c8db689edb7b7ac5076a5a7598d34fb24a97
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-22 12:23:16 -0700

    changed port

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import axios from "axios";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -11,12 +11,12 @@ const App = () => {
 
   const getMovieList = () => {
     axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
+      .get("http://localhost:5003/api/movies")
+      .then((res) => setMovieList(res.data))
+      .catch((err) => console.log(err.response));
   };
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
